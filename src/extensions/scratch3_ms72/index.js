d52bff460baa588276710ac4f1a8c48202f4c22b
M-S-72 <s.marten72@icloud.com>
2019-02-03 15:02:55 +0100

    M_S_72 extension: new blocks

--- src/extensions/scratch3_ms72/index.js
+++ src/extensions/scratch3_ms72/index.js
@@ -50,7 +50,65 @@ class Scratch3MS72Blocks {
                             })
                         }
                     }
-               }
+               },
+               {
+                    opcode: 'trueblock',
+                    blockType: BlockType.BOOLEAN,
+                    text: formatMessage({
+                        id: 'ms72.true',
+                        default: 'true',
+                        description: 'Name of the ms72.true block. '
+                    })
+                },
+                {
+                    opcode: 'falseblock',
+                    blockType: BlockType.BOOLEAN,
+                    text: formatMessage({
+                        id: 'ms72.false',
+                        default: 'false',
+                        description: 'Name of the ms72.false block. '
+                    })
+                },
+                {
+                    opcode: 'ifthenelse',
+                    blockType: BlockType.REPORTER,
+                    text: formatMessage({
+                        id: 'ms72.block.ifthenelse.text',
+                        default: 'if [BOOLEAN] then [STRING1] else [STRING2]',
+                        description: 'Name of the ms72.ifthenelse block. '
+                    }),
+                    arguments: {
+                        BOOLEAN: {
+                            type: ArgumentType.BOOLEAN,
+                        },
+                        STRING1: {
+                            type: ArgumentType.STRING,
+                            defaultValue: formatMessage({
+                                id: 'ms72.block.ifthenelse.then.default',
+                                default: 'apple',
+                                description: 'apple: the default text'
+                            })
+                        },
+                        STRING2: {
+                            type: ArgumentType.STRING,
+                            defaultValue: formatMessage({
+                                id: 'ms72.block.ifthenelse.else.default',
+                                default: 'banana',
+                                description: 'banana: the default text'
+                            })
+                        }
+
+                    }
+
+                },{
+                    opcode: 'pi',
+                    blockType: BlockType.REPORTER, 
+                    text: formatMessage({
+                        id: 'ms72.block.pi.text',
+                        default: 'Pi',
+                        description: 'Name of the ms72.pi block. '
+                    }),
+                }
             ],
             menus: {
             },
@@ -58,9 +116,15 @@ class Scratch3MS72Blocks {
              de: {
                  'extensionName': 'M_S_72 Test',
                  'ms72.categoryName': 'M_S_72',
+                 'ms72.true': 'wahr',
+                 'ms72.false': 'falsch',
                  'ms72.block.testblock1': 'Testblock 1',
                  'ms72.block.text': '[STRING]',
-                 'ms72.block.text.default': 'Hallo'
+                 'ms72.block.text.default': 'Hallo',
+                 'ms72.block.ifthenelse.text': 'falls [BOOLEAN], dann [STRING1], sonst [STRING2]',
+                 'ms72.block.ifthenelse.then.default': 'Apfel',
+                 'ms72.block.ifthenelse.else.default': 'Banane',
+                 'ms72.block.pi.text': 'Pi'
              }
             }
         };
@@ -71,6 +135,23 @@ class Scratch3MS72Blocks {
     text (args) {
      
      return args.STRING;
+    }
+    trueblock () {
+     
+     return true;
+    }
+    falseblock () {
+     
+     return false;
+    }
+    ifthenelse (args) {
+     
+     if(args.BOOLEAN) return args.STRING1;
+     else return args.STRING2;
+    }
+    pi () {
+     
+     return 3.1415926535897932384626433832795;
     }
 }
 
